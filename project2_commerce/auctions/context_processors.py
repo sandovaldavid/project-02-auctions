286e2b1328d382588ed106373c4a5217cd942e79
Juan David Sandoval Salvador <102970701+sandovaldavid@users.noreply.github.com>
2024-12-04 17:53:18 -0500

    Delete project2_commerce directory_docker

--- project2_commerce/auctions/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Watchlist
-
-
-def watchlist_count(request):
-    if request.user.is_authenticated:
-        count = Watchlist.objects.filter(user=request.user, active=True).count()
-    else:
-        count = 0
-    return {'watchlist_count': count}
