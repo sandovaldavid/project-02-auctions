ea182ef48fad47fc0d7c803d40eb059a2baa3097
sandovaldavid <sandovaldavid2201@gmail.com>
2024-12-28 03:50:46 -0500

    🚧 Update URL patterns to include trailing slashes for consistency

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,15 +4,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-    path("addAuctions", views.new_auctions, name="addAuctions"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register, name="register"),
+    path("addAuctions/", views.new_auctions, name="addAuctions"),
     path("listing/<int:listing_id>", views.listing, name="listing"),
     path("bid/<int:listing_id>", views.bid, name="bid"),
     path("watchlist/<int:listing_id>", views.watchlist, name="watchlist"),
     path("Watchlist_remove/<int:listing_id>", views.watchlist_remove, name="watchlist_remove"),
     path('listing/<int:listing_id>/close', views.close_auction, name='close_auction'),
-    path('categories', views.categories, name='categories'),
+    path('categories/', views.categories, name='categories'),
     path('comment/<int:listing_id>', views.comment, name='comment'),
 ]
