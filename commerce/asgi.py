d04cce9c6af3ac35a7dd9b323d54ed7c4f72b00b
sandovaldavid <sandovaldavid2201@gmail.com>
2025-09-03 20:34:02 -0500

    feat(websocket): add websocket support for notifications
    Integrate Channels to handle websocket connections with authentication middleware

--- commerce/asgi.py
+++ commerce/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import auctions.notifications.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'commerce.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commerce.settings")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(auctions.notifications.routing.websocket_urlpatterns)
+        ),
+    }
+)
