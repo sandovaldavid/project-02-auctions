2c120238b39691350831aafae0f72237b02726b3
sandovaldavid <sandovaldavid2201@gmail.com>
2025-09-03 20:32:03 -0500

    refactor(auctions): set default auto field and import signals in ready
    Add default_auto_field configuration and import signals module in ready method to ensure proper initialization

--- auctions/apps.py
+++ auctions/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class AuctionsConfig(AppConfig):
     name = "auctions"
+    default_auto_field = "django.db.models.BigAutoField"
+
+    def ready(self):
+        import auctions.notifications.signals  # noqa: F401
